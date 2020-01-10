9ba2ab842c68c90159a31127b1a771b9e9ee4ce1
Ted <weifeng.ang@digipen.edu>
2020-01-10 16:59:32 +0800

    Added yannick code as our starting template
    - This will be the base for master branch for now. As we experiment more changes on different branches, we shall update the master branch with working code only.

--- SpaceGame/src/Main.cpp
+++ SpaceGame/src/Main.cpp
@@ -1,7 +1,104 @@
-#include <iostream>
+// ---------------------------------------------------------------------------
+// includes
 
-int main()
+#include "AEEngine.h"
+
+// ---------------------------------------------------------------------------
+// Libraries
+//#pragma comment (lib, "Alpha_Engine.lib")
+
+
+// ---------------------------------------------------------------------------
+// main
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
 {
-	std::cout << "Hello world!" << std::endl;
-	std::cin.get();
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	///////////////////////
+	// Variable declaration
+
+	int gGameRunning = 1;
+
+	// Variable declaration end
+	///////////////////////////
+
+
+	/////////////////
+	// Initialization
+
+	// Using custom window procedure
+	AESysInit(hInstance, nCmdShow, 800, 600, 1, 60, true, NULL);
+
+
+	// Changing the window title
+	AESysSetWindowTitle("Master Branch");
+
+	// reset the system modules
+	AESysReset();
+
+	// Initialization end
+	/////////////////////
+
+
+
+	////////////////////////////////
+	// Creating the objects (Shapes)
+
+	// Creating the objects (Shapes) end
+	////////////////////////////////////
+
+
+
+	////////////////////////////
+	// Loading textures (images)
+
+	// Loading textures (images) end
+	//////////////////////////////////
+
+	//////////////////////////////////
+	// Creating Fonts	
+
+	// Creating Fonts end
+	//////////////////////////////////
+
+
+	// Game Loop
+	while (gGameRunning)
+	{
+		// Informing the system about the loop's start
+		AESysFrameStart();
+
+		// Handling Input
+		AEInputUpdate();
+
+		///////////////////
+		// Game loop update
+
+		// Game loop update end
+		///////////////////////
+
+
+		//////////////////
+		// Game loop draw
+
+		// Game loop draw end
+		/////////////////////
+
+
+		// Informing the system about the loop's end
+		AESysFrameEnd();
+
+		// check if forcing the application to quit
+		if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
+			gGameRunning = 0;
+	}
+
+
+	// free the system
+	AESysExit();
 }
