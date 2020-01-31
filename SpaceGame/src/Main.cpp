1e75e5579bca0dd2152f0027eb0217f9735524a3
Farzaana <farzaanaxd@gmail.com>
2020-01-31 15:39:27 +0800

    Revert "File Commit"
    This reverts commit e92334a057c000802308df41fd2f43ce0f311ed7.

--- SpaceGame/src/Main.cpp
+++ SpaceGame/src/Main.cpp
@@ -3,12 +3,9 @@
 
 #include "AEEngine.h"
 
-
-
 // ---------------------------------------------------------------------------
 // Libraries
 //#pragma comment (lib, "Alpha_Engine.lib")
-
 
 
 // ---------------------------------------------------------------------------
@@ -16,8 +13,8 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow)
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
@@ -26,28 +23,24 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	// Variable declaration
 
 	int gGameRunning = 1;
-	AEGfxTexture* pTex1;
 
 	// Variable declaration end
 	///////////////////////////
-	AEGfxVertexList* pMesh1 = 0;
-	AEGfxVertexList* pMesh2 = 0;
+
 
 	/////////////////
 	// Initialization
-	//set background color 
 
 	// Using custom window procedure
-	AESysInit(hInstance, nCmdShow, 1280, 768, 1, 60, true, NULL);
+	AESysInit(hInstance, nCmdShow, 1280, 720, 1, 60, true, NULL);
 
 
 	// Changing the window title
-	AESysSetWindowTitle("My New Demo!");
+	AESysSetWindowTitle("Master Branch");
 
 	// reset the system modules
 	AESysReset();
 
-	AEGfxSetBackgroundColor(0.0f, 0.0f, 0.0f);
 	// Initialization end
 	/////////////////////
 
@@ -55,39 +48,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	////////////////////////////////
 	// Creating the objects (Shapes)
-	// Informing the library that we're about to start adding triangles
-	AEGfxMeshStart();
 
-	// 1 triangle at a time
-	// X, Y, Color, texU, texV
-	AEGfxTriAdd(
-		-25.5f, -25.5f, 0xFFFF0000, 0.0f, 0.0f,
-		25.5f, 0.0f, 0xFFFF0000, 0.0f, 0.0f,
-		-25.5f, 25.5f, 0xFFFF0000, 0.0f, 0.0f);
-
-	// Saving the mesh (list of triangles) in pMesh1
-
-	pMesh1 = AEGfxMeshEnd();
-	AE_ASSERT_MESG(pMesh1, "Failed to create mesh 1!!");
-
-	// Informing the library that we're about to start adding triangles
-	AEGfxMeshStart();
-
-	// This shape has 2 triangles
-	AEGfxTriAdd(
-		-30.0f, -30.0f, 0x00FF00FF, 0.0f, 1.0f,
-		30.0f, -30.0f, 0x00FFFF00, 1.0f, 1.0f,
-		-30.0f, 30.0f, 0x0000FFFF, 0.0f, 0.0f);
-
-	AEGfxTriAdd(
-		30.0f, -30.0f, 0x00FFFFFF, 1.0f, 1.0f,
-		30.0f, 30.0f, 0x00FFFFFF, 1.0f, 0.0f,
-		-30.0f, 30.0f, 0x00FFFFFF, 0.0f, 0.0f);
-
-	// Saving the mesh (list of triangles) in pMesh2
-
-	pMesh2 = AEGfxMeshEnd();
-	AE_ASSERT_MESG(pMesh2, "Failed to create mesh 2!!");
 	// Creating the objects (Shapes) end
 	////////////////////////////////////
 
@@ -95,8 +56,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	////////////////////////////
 	// Loading textures (images)
-	pTex1 = AEGfxTextureLoad("PlanetTexture.png"); 
-	AE_ASSERT_MESG(pTex1, "Failed to create texture1!!")
+
 	// Loading textures (images) end
 	//////////////////////////////////
 
@@ -118,58 +78,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 		///////////////////
 		// Game loop update
-		// Blending mode
-		if (AEInputCheckCurr(AEVK_1))
-			AEGfxSetBlendMode(AE_GFX_BM_NONE);
-		else if (AEInputCheckCurr(AEVK_2))
-			AEGfxSetBlendMode(AE_GFX_BM_BLEND);
-		else if (AEInputCheckCurr(AEVK_3))
-			AEGfxSetBlendMode(AE_GFX_BM_ADD);
+
 		// Game loop update end
 		///////////////////////
 
 
 		//////////////////
 		// Game loop draw
-		// Drawing object 1
-		AEGfxSetRenderMode(AE_GFX_RM_COLOR);
-		// Set position for object 1
-		AEGfxSetPosition(0.0f, 0.0f);
-		// No texture for object 1
-		AEGfxTextureSet(NULL, 0, 0);
-		//add tint 
-		AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
-		// Drawing the mesh (list of triangles)
-		AEGfxMeshDraw(pMesh1, AE_GFX_MDM_TRIANGLES);
-		
-
-		// Drawing object 2 - (first) - No tint
-		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
-		// Drawing object 2 - (first) - No tint
-		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
-		// Set position for object 2
-		AEGfxSetPosition(100.0f, -60.0f);
-		// No tint
-		AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
-		// Set texture
-		AEGfxTextureSet(pTex1, 0.0f, 0.0f);
-		// Drawing the mesh (list of triangles)
-		AEGfxMeshDraw(pMesh2, AE_GFX_MDM_TRIANGLES);
-		// Set Transparency
-		AEGfxSetTransparency(1.0f);
-
-		// Drawing object 3 - (Second) - Blue tint
-		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
-		// Set position for object 3
-		AEGfxSetPosition(100.0f, 60.0f);
-		// Add Blue tint
-		AEGfxSetTintColor(0.0f, 0.0f, 1.0f, 1.0f);
-		// Set texture
-		AEGfxTextureSet(pTex1, 0.0f, 0.0f);
-		// Drawing the mesh (list of triangles)
-		AEGfxMeshDraw(pMesh2, AE_GFX_MDM_TRIANGLES);
-		// Set Transparency
-		AEGfxSetTransparency(1.0f);
 
 		// Game loop draw end
 		/////////////////////
@@ -183,8 +98,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 			gGameRunning = 0;
 	}
 
-	AEGfxMeshFree(pMesh1);
-	AEGfxTextureUnload(pTex1);
 
 	// free the system
 	AESysExit();
