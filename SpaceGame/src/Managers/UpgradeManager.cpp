9975766472bd3ef942a82b9cd8ac3f1e5071ca8c
99magic <56583138+99magic@users.noreply.github.com>
2020-03-12 22:18:12 +0800

    Upgrade Manager
    Allows player to upgrade their stats and weapons

--- SpaceGame/src/Managers/UpgradeManager.cpp
+++ SpaceGame/src/Managers/UpgradeManager.cpp
@@ -6,85 +6,143 @@ namespace UpgradeManager
 	//private containers
 	bool isLoaded = false;
 	std::unordered_map<UpgradePackages, PlayerUpgrade_Base*> database_playerUpgrade;
-	std::unordered_map<UpgradePackages, WeaponUpgrade_BaseRange*> database_rangeUpgrade;
-	std::unordered_map<UpgradePackages, WeaponUpgrade_BaseMelee*> database_meleeUpgrade;
+	std::unordered_map<UpgradePackages, WeaponUpgradeRange_BaseRange*> database_rangeUpgrade;
+	std::unordered_map<UpgradePackages, WeaponUpgradeMelee_BaseMelee*> database_meleeUpgrade;
+
+	int upgrade1 = -1;
+	int upgrade2 = -1;
+	int upgrade3 = -1;
 
 	void Init_UpgradeDatabase()
 	{
 		if (!isLoaded)
 		{
-			database_playerUpgrade.emplace(UpgradePackages::Player_V1, new PlayerUpgrade_V1);
-			database_rangeUpgrade.emplace(UpgradePackages::Range_Pistol, new WeaponUpgrade_Pistol);
-			database_meleeUpgrade.emplace(UpgradePackages::Melee_Normal, new WeaponUpgrade_Normal);
+			database_playerUpgrade.emplace(UpgradePackages::PlayerUpgrade_HpUp1, new PlayerUpgrade_HpUp1);
+			database_playerUpgrade.emplace(UpgradePackages::PlayerUpgrade_LifeUp1, new PlayerUpgrade_LifeUp1);
+			database_playerUpgrade.emplace(UpgradePackages::PlayerUpgrade_ShieldUp1, new PlayerUpgrade_ShieldUp1);
+			database_playerUpgrade.emplace(UpgradePackages::PlayerUpgrade_ThrustAccelUp1, new PlayerUpgrade_ThrustAccelUp1);
+
+
+
+			database_rangeUpgrade.emplace(UpgradePackages::RangeWeaponUpgrade_FireRateDown1, new RangeWeaponUpgrade_FireRateDown1);
+			database_rangeUpgrade.emplace(UpgradePackages::RangeWeaponUpgrade_ReloadRateDown1, new RangeWeaponUpgrade_ReloadRateDown1);
+			database_rangeUpgrade.emplace(UpgradePackages::RangeWeaponUpgrade_AmmoUp1, new RangeWeaponUpgrade_AmmoUp1);
+			database_rangeUpgrade.emplace(UpgradePackages::RangeWeaponUpgrade_DamageUp1, new RangeWeaponUpgrade_DamageUp1);
+			database_rangeUpgrade.emplace(UpgradePackages::RangeWeaponUpgrade_SpreadDown1, new RangeWeaponUpgrade_SpreadDown1);
+			database_rangeUpgrade.emplace(UpgradePackages::RangeWeaponUpgrade_BulletSizeUp1, new RangeWeaponUpgrade_BulletSizeUp1);
+			database_rangeUpgrade.emplace(UpgradePackages::RangeWeaponUpgrade_BulletSpeedUp1, new RangeWeaponUpgrade_BulletSpeedUp1);
+
+			database_meleeUpgrade.emplace(UpgradePackages::MeleeWeaponUpgrade_RangeUp1, new MeleeWeaponUpgrade_RangeUp1);
+			database_meleeUpgrade.emplace(UpgradePackages::MeleeWeaponUpgrade_DamageUp1, new MeleeWeaponUpgrade_DamageUp1);
+			//database_rangeUpgrade.emplace(UpgradePackages::Range_Pistol, new WeaponUpgrade_Pistol);
+			//database_meleeUpgrade.emplace(UpgradePackages::Melee_Normal, new WeaponUpgrade_Normal);
+
+
 			isLoaded = true;
 		}
 	}
 
-	void PlayerUpgrade(cSpaceShip* spaceship, cHealth* health, UpgradePackages upgradePack, int level)
+	void PlayerUpgrade(cSpaceShip* spaceship, cHealth* health, UpgradePackages upgradePack)
 	{
 		PlayerUpgrade_Base* data = database_playerUpgrade[upgradePack];
-		switch (level)
-		{
-			case 1:
-				spaceship->_thrustDelay		+= data->Get_U1_ThrustDelay();
-				spaceship->_lives			+= data->Get_U1_LifeIncrease();
-				health->_healthMax			+= data->Get_U1_HealthIncrease();
-				health->_shieldMax			+= data->Get_U1_ShieldIncrease();
-				break;
-			case 2:
-				spaceship->_thrustDelay		+= data->Get_U2_ThrustDelay();
-				spaceship->_lives			+= data->Get_U2_LifeIncrease();
-				health->_healthMax			+= data->Get_U2_HealthIncrease();
-				health->_shieldMax			+= data->Get_U2_ShieldIncrease();
-				break;
-		}
 		
+		spaceship->_thrustDelay			+= data->Get_ThrustAcceleration();
+		spaceship->_lives				+= data->Get_LifeIncrease();
+		health->_healthMax				+= data->Get_ShieldIncrease();
+		health->_shieldMax				+= data->Get_HealthIncrease();
+
 	}
 
-	void WeaponUpgradeRange(cRangeWeapon* rangeWeapon, UpgradePackages upgradePack, int level)
+	void WeaponUpgradeRange(cRangeWeapon* rangeWeapon, UpgradePackages upgradePack)
 	{
-		WeaponUpgrade_BaseRange* data = database_rangeUpgrade[upgradePack];
-		switch (level)
+		WeaponUpgradeRange_BaseRange* data = database_rangeUpgrade[upgradePack];
+
+			rangeWeapon->_fireRate			-= data->Get_FireRateDecrease();
+			rangeWeapon->_reloadRate		-= data->Get_ReloadRateDecrease();
+			rangeWeapon->_ammo				+= data->Get_AmmoIncrease();
+			rangeWeapon->_damage			+= data->Get_DamageIncrease();
+			rangeWeapon->_shootingSpread	-= data->Get_SpreadDecrease();
+			rangeWeapon->_bulletSize		-= data->Get_BulletSizeIncrease();
+			rangeWeapon->_bulletSpeed		+= data->Get_BulletSpeedIncrease();
+	}
+
+	void WeaponUpgradeMelee(cMeleeWeapon* meleeWeapon, UpgradePackages upgradePack)
+	{
+		WeaponUpgradeMelee_BaseMelee* data = database_meleeUpgrade[upgradePack];
+
+			meleeWeapon->_meleeRange += data->Get_RangeIncrease();
+			meleeWeapon->_damage += data->Get_DamageIncrease();
+
+	}
+
+	int RandomUpgrade()
+	{
+		int randomUpgradeType = rand() % NUMBER_OF_UPGRADES_TYPE + 1;
+		int randomUpgrade = 0;
+		bool isUnique = false;
+
+		while (isUnique)
 		{
-		case 1:
-			rangeWeapon->_fireRate			+= data->Get_U1_FireRate();
-			rangeWeapon->_reloadRate		+= data->Get_U1_ReloadRate();
-			rangeWeapon->_ammo				+= data->Get_U1_Ammo();
-			rangeWeapon->_damage			+= data->Get_U1_Damage();
-			break;
-		case 2:
-			rangeWeapon->_fireRate			+= data->Get_U2_FireRate();
-			rangeWeapon->_reloadRate		+= data->Get_U2_ReloadRate();
-			rangeWeapon->_ammo				+= data->Get_U2_Ammo();
-			rangeWeapon->_damage			+= data->Get_U2_Damage();
-			break;
+			if (randomUpgradeType == 1) // Player Upgrade
+			{
+				randomUpgrade = rand() % NUMBER_OF_PLAYERPGRADES;
+			}
+			else if (randomUpgradeType == 2) // Range Weapon Upgrade
+			{
+				randomUpgrade = rand() % NUMBER_OF_RANGEWEAPONUPGRADES + NUMBER_OF_PLAYERPGRADES;
+			}
+			else if (randomUpgradeType == 3) // Melee Weapon Upgrade
+			{
+				randomUpgrade = rand() % NUMBER_OF_MELEEWEAPONUPGRADES + NUMBER_OF_RANGEWEAPONUPGRADES + NUMBER_OF_PLAYERPGRADES;
+			}
 		}
+
+		return randomUpgrade;
 	}
 
-	void WeaponUpgradeMelee(cMeleeWeapon* meleeWeapon, UpgradePackages upgradePack, int level)
+	bool CheckUnique()
 	{
-		WeaponUpgrade_BaseMelee* data = database_meleeUpgrade[upgradePack];
-
-		switch (level)
+		if (upgrade1 == upgrade2 || upgrade1 == upgrade3
+			|| upgrade2 == upgrade3)
 		{
-		case 1:
-			meleeWeapon->_meleeRange		+= data->Get_U1_MeleeRange();
-			meleeWeapon->_damage			+= data->Get_U1_Damage();
-			break;
+			return false;
 		}
+		return true;
 	}
 
-	void WeaponChange(cRangeWeapon* rangeWeapon, WeaponType weaponType, UpgradePackages upgradePack)
+	void ClearAllUpgradeChoice()
 	{
-		rangeWeapon->_currWeapon = weaponType;
-		//Reset the upgrade values
-		WeaponUpgrade_BaseRange* data = database_rangeUpgrade[upgradePack];
-
-		rangeWeapon->_fireRate				= data->Get_U0_FireRate();
-		rangeWeapon->_reloadRate			= data->Get_U0_ReloadRate();
-		rangeWeapon->_ammo					= data->Get_U0_Ammo();
-		rangeWeapon->_damage				= data->Get_U0_Damage();
+		upgrade1 = -1;
+		upgrade2 = -1;
+		upgrade3 = -1;
 	}
+
+	
+
+	//void WeaponUpgradeMelee(cMeleeWeapon* meleeWeapon, UpgradePackages upgradePack, int level)
+	//{
+	//	WeaponUpgrade_BaseMelee* data = database_meleeUpgrade[upgradePack];
+
+	//	switch (level)
+	//	{
+	//	case 1:
+	//		meleeWeapon->_meleeRange		+= data->Get_U1_MeleeRange();
+	//		meleeWeapon->_damage			+= data->Get_U1_Damage();
+	//		break;
+	//	}
+	//}
+
+	//void WeaponChange(cRangeWeapon* rangeWeapon, WeaponType weaponType, UpgradePackages upgradePack)
+	//{
+	//	rangeWeapon->_currWeapon = weaponType;
+	//	//Reset the upgrade values
+	//	WeaponUpgrade_BaseRange* data = database_rangeUpgrade[upgradePack];
+
+	//	rangeWeapon->_fireRate				= data->Get_U0_FireRate();
+	//	rangeWeapon->_reloadRate			= data->Get_U0_ReloadRate();
+	//	rangeWeapon->_ammo					= data->Get_U0_Ammo();
+	//	rangeWeapon->_damage				= data->Get_U0_Damage();
+	//}
 
 	void Cleanup_UpgradeDatabase()
 	{
